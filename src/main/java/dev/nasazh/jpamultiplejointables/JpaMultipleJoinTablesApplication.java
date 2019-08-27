a5e6575fb9a480db22ecbed6d733e22c9a66b2da
Aidas <nasazh@outlook.com>
2019-08-27 21:23:53 +0300

    Spring Initializr

---
+++ src/main/java/dev/nasazh/jpamultiplejointables/JpaMultipleJoinTablesApplication.java
@@ -0,0 +1,13 @@
+package dev.nasazh.jpamultiplejointables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMultipleJoinTablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMultipleJoinTablesApplication.class, args);
+	}
+
+}
